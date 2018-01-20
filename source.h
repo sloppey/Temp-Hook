edaf2b868c640374c97372828cf0941bea83d8d7
sloppey <sloppeylole@gmail.com>
2018-01-19 20:58:09 -0500

    Create source.h

---
+++ source.h
@@ -0,0 +1,11 @@
+
+  void* Mem_Edit(DWORD address, void* hook, bool revert=false){
+		DWORD oldprot;
+		if (!revert){
+		    //placing hook
+			return (void*)0;
+		}
+		else{
+		    //reverting back to prevent roblox from detecting our hook
+		}
+	}
